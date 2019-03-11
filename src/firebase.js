16500f352ed4f6179b6a5c3c36ff970d05e0ffc6
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2019-03-11 11:59:05 -0700

    initial

---
+++ src/firebase.js
@@ -0,0 +1,7 @@
+var config = {
+    apiKey: 'AIzaSyDNbhKqMmp7PjQYd1LV_hR_DPKZySfb--Y',
+    authDomain: 'project1-20f15.firebaseapp.com'
+};
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
